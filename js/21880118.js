2de3766dce6d82c18ce925ea103627e3ccf23924
Suong Tran <tranthusuongg@gmail.com>
2022-04-30 03:29:56 +0700

    make it better

---
+++ js/21880118.js
@@ -0,0 +1,16 @@
+function loadData(request, targetId, sourcrId) {
+    $.ajax({
+        url: 'http://web1-api.herokuapp.com/api/' + request,
+        cache: false,
+        success: function (data) {
+            let jsonData = {
+                data: data
+            };
+            let target = $(targetId);
+            let source = $(sourcrId).html();
+            let template = Handlebars.compile(source);
+            $(target).html(template(jsonData));
+            console.log(data);
+        }
+    });
+}
